98b58093b52cf50c29257a2c43feb88b804f41b1
Tae Hyun Jo <taehyun.jo.90@gmail.com>
2020-10-04 00:40:32 +0900

    시간초과 테스트1

--- Lab4/Lab4/MultiSet.cs
+++ Lab4/Lab4/MultiSet.cs
@@ -144,9 +144,9 @@ namespace Lab4
         public List<MultiSet> FindPowerSet()
         {
             List<MultiSet> powerSet = new List<MultiSet>();
-            HashSet<string> remainElements = new HashSet<string>(this.ToList());
-            this.findPowerSetRecursive(powerSet, new MultiSet(), remainElements.ToList());
-            powerSet.Sort(this.compare);
+            //HashSet<string> remainElements = new HashSet<string>(this.ToList());
+            //this.findPowerSetRecursive(powerSet, new MultiSet(), remainElements.ToList());
+            //powerSet.Sort(this.compare);
             return powerSet;
         }
 
